23966063f57c3b5b1c4cf6da92caa40ce3fde791
hxcccc <18659272095@163.com>
2019-12-20 10:40:29 +0800

    下载接口

--- main.go
+++ main.go
@@ -10,6 +10,7 @@ func main() {
 	http.HandleFunc("/file/upload", handle.UploadHandler)
 	http.HandleFunc("/file/upload/succ", handle.UploadSuccHandler)
 	http.HandleFunc("/file/meta", handle.GetFileMetaHandler)
+	http.HandleFunc("/file/download", handle.DownLoadHandler)
 	err := http.ListenAndServe(":8888", nil)
 	if err != nil {
 		log.Panic(err)
