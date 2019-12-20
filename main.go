5f821cc73ed9e3f79fa0c8298683e1140c55cb26
hxcccc <18659272095@163.com>
2019-12-20 15:46:21 +0800

    v2

--- main.go
+++ main.go
@@ -12,7 +12,7 @@ func main() {
 	http.HandleFunc("/file/meta", handle.GetFileMetaHandler)
 	http.HandleFunc("/file/download", handle.DownLoadHandler)
 	http.HandleFunc("/file/update", handle.FileMetaUploadHandler)
-	http.HandleFunc("file/delete", handle.FileDelHandler)
+	http.HandleFunc("/file/delete", handle.FileDelHandler)
 	err := http.ListenAndServe(":8888", nil)
 	if err != nil {
 		log.Panic(err)
