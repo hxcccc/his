2f8592f948e14812d43c18cd174c454da256e598
hxcccc <18659272095@163.com>
2020-01-04 19:09:55 +0800

    访稳静态文件路径

--- main.go
+++ main.go
@@ -7,6 +7,7 @@ import (
 )
 
 func main() {
+	http.Handle("/static/", http.StripPrefix("/static/", http.FileServer(http.Dir("./static"))))
 	http.HandleFunc("/file/upload", handle.UploadHandler)
 	http.HandleFunc("/file/upload/succ", handle.UploadSuccHandler)
 	http.HandleFunc("/file/meta", handle.GetFileMetaHandler)
