4e59ff6508a96d2e9d47ab147e2cc4b14671661a
hxcccc <18659272095@163.com>
2020-01-08 15:26:58 +0800

    响应数据

--- handle/user.go
+++ handle/user.go
@@ -117,14 +117,14 @@ func IsTokenValid(username string, token string) bool {
 		return false
 	}
 	//判断token的时效性是否过期
-	tokenTs, err := strconv.Atoi(string(token[len(token)-8:]))
+	tokenTs, err := strconv.ParseInt(string(token[len(token)-8:]), 10, 64)
 	if err != nil {
-		fmt.Println("ts:string to int failed")
+		fmt.Println("ts:string to int64 failed")
 		return false
 	}
-	nowTs, err := strconv.Atoi(fmt.Sprintf("%x", time.Now().Unix())[:8])
+	nowTs, err := strconv.ParseInt(fmt.Sprintf("%x", time.Now().Unix())[:8], 10, 64)
 	if err != nil {
-		fmt.Println("ts:string to int failed")
+		fmt.Println("ts:string to int64 failed")
 		return false
 	}
 	if keepTs := nowTs - tokenTs;keepTs > 86400 {
